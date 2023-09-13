4b7d2192881a2fbe28fb70c9b11a2cad3e7affbb
Tyler Long <tyler4long@gmail.com>
2023-09-13 10:34:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RingCentral telephony session filter test
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
+  });
+  const ext = await rc.restapi().account().extension().get();
+  console.log(JSON.stringify(ext, null, 2));
+  await rc.revoke();
+};
+main();
